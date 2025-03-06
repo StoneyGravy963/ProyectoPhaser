c493f04b222d31a9dde03320dbd1c1928b468b89
StoneyGravy963 <al210356@edu.uaa.mx>
2025-03-05 18:35:08 -0600

    Plantilla Principal

---
+++ recursos/config.js
@@ -0,0 +1,15 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
+    scene: [Juego] 
+};
+
+const game = new Phaser.Game(config);
