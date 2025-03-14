0f297d28a5abeb317d741123a8553e602d6d4218
jaime López <al232901@edu.uaa.mx>
2025-03-14 11:58:55 -0600

    Revert "refactor"
    This reverts commit 3251fcec9aff319e588ae06e4c917907ab5890f4.

--- recursos/juego.js
+++ recursos/juego.js
@@ -433,10 +433,16 @@ class Juego extends Phaser.Scene {
         }
 
         // pasar al siguiente nivel
+<<<<<<< HEAD
+        // if (this.player.sprite.x >= 1100 - 900) {//cambiar ancho 
+        //     this.scene.start('Boss', { score: this.score, vidas: this.vidas });
+        // }
+=======
         if (this.player.sprite.x >= 1100 - 900) {//cambiar ancho 
             this.musicaF.destroy();
             this.scene.start('Boss', { score: this.score, vidas: this.vidas });
         }
+>>>>>>> 3f2accfe156f4f0240c7ae9ccd92b9a1f6c5c7b6
     }
 
     hitEnemy(ataque, enemy) {
