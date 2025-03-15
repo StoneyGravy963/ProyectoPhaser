d544d3bfc64413d6d6cfb9e603c7fa1e1996986c
jaime López <al232901@edu.uaa.mx>
2025-03-14 21:56:07 -0600

    FALTA NIVEL 1

--- recursos/boss.js
+++ recursos/boss.js
@@ -26,7 +26,7 @@ class Boss extends Phaser.Scene {
     }
 
     create() {
-        let back=this.add.image(600,300, 'back').setScale(1.5).setPosition(1100,400);
+        let back=this.add.image(600,300, 'back').setScale(1.5).setPosition(600,400);
         back.setAlpha(0.6);
 
         this.musicaF = this.sound.add('background', { loop: true});
@@ -34,8 +34,8 @@ class Boss extends Phaser.Scene {
         // sonido
         this.controlMusica = new Musica(this, this.musicaF);
         
-        this.physics.world.setBounds(0, 0, 1500, 800);
-        this.cameras.main.setBounds(0, 0, 1500, 800);
+        this.physics.world.setBounds(0, 0, 1200, 800);
+        // this.cameras.main.setBounds(0, 0, 1500, 800);
 
         //SPRITES
         this.anims.create({
@@ -78,8 +78,13 @@ class Boss extends Phaser.Scene {
         this.platforms.create(400+1999, 730, 'suelo').refreshBody();
         this.platforms.create(200, 600, 'plataforma'); 
         this.platforms.create(900, 600, 'plataforma'); 
-        this.platforms.create(550, 450, 'plataforma'); 
+        this.platforms.create(500, 450, 'plataforma'); 
         this.platforms.create(900, 600, 'plataforma'); 
+
+        this.platforms.create(200, 350, 'plataforma').setDisplaySize(200,35).setSize(200,35).setOffset(100,0); 
+
+        this.platforms.create(1050, 450, 'plataforma').setDisplaySize(200,35).setSize(200,35).setOffset(100,0); 
+ 
         
         this.player = new Player(this, 100, 600);
         this.cameras.main.startFollow(this.player.sprite, false,0.2,0,0,200);
